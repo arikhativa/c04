19a20df53214f0d60bbcbdfd0dc6549c66b5b5eb
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-25 10:06:24 +0200

    changed putnbr

--- ex02/ft_putnbr.c
+++ ex02/ft_putnbr.c
@@ -6,63 +6,37 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 11:45:50 by yrabby            #+#    #+#             */
-/*   Updated: 2022/05/24 11:45:52 by yrabby           ###   ########.fr       */
+/*   Updated: 2022/05/25 10:06:11 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	count_digit(int n)
+void	ft_putchar(char c)
 {
-	int	c;
-
-	c = 0;
-	if (n == 0)
-		return (1);
-	while (n)
-	{
-		n /= 10;
-		++c;
-	}
-	return (c);
+	write(1, &c, 1);
 }
 
-int	power(int n, int p)
+void	print(long n)
 {
-	int	ret;
-
-	ret = n;
-	if (p == 0)
-		return (1);
-	while (p > 1)
+	if (n / 10)
 	{
-		ret *= n;
-		--p;
+		print(n / 10);
+		print(n % 10);
 	}
-	return (ret);
+	if (n < 10)
+		ft_putchar(n + '0');
 }
 
 void	ft_putnbr(int nb)
 {
-	int		p;
-	char	ch;
+	long	n;
 
-	if (nb == -2147483648)
+	n = (long)nb;
+	if (n < 0)
 	{
-		write(1, "-2147483648", 11);
-		return ;
-	}
-	if (nb < 0)
-	{
-		nb = nb * -1;
+		n = n * -1;
 		write(1, "-", 1);
 	}
-	p = power(10, count_digit(nb) - 1);
-	while (p)
-	{
-		ch = (nb / p) + '0';
-		write(1, &ch, 1);
-		nb %= p;
-		p /= 10;
-	}
+	print(n);
 }
